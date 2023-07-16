68d2d36a9f2e15979bd5a02561746495e0d8b898
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-16 14:04:52 +0530

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,68 +1,77 @@
 #include <iostream>
+#include <memory_resource>
 
-int main(void) {
-
+void print_menu() {
     // 1 print help
     std::cout << "1. Print help " << std::endl;
-
     // 2 print exchange stat
     std::cout << "2. Print exchange stats " << std::endl;
-
     // 3 make an offer
     std::cout << "3. Make an offer " << std::endl;
-
     // 4 make a build
     std::cout << "4. Make a bid " << std::endl;
-
     // 5 print wallet
     std::cout << "5. Print wallet " << std::endl;
-
     // 6 continue
     std::cout << "6. Continue " << std::endl;
 
     std::cout << "=====================" << std::endl;
+}
+
+int get_user_option() {
 
     int user_option = 0;
+    std::cout << "Type in 1-6: ";
+    std::cin >> user_option;
+    std::cout << "You choose: " << user_option << std::endl;
 
-    // taking user input
+    return user_option;
+}
 
+void process_user_option(int user_option) {
+
+    if (user_option == 0) {
+        std::cout << "Invalid choice. Choose from 1-6." << std::endl;
+    }
+    if (user_option == 1) {
+        std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers." << std::endl;
+    }
+    if (user_option == 2) {
+        std::cout << "Market looks good." << std::endl;
+    }
+    if (user_option == 3) {
+        std::cout << "Mark and offer - enter the amount." << std::endl;
+    }
+    if (user_option == 4) {
+        std::cout << "Make a bid - enter the amount." << std::endl;
+    }
+    if (user_option == 5) {
+        std::cout << "Your wallet is empty." << std::endl;
+    }
+    if (user_option == 6) {
+        std::cout << "Going to next time frame." << std::endl;
+    }
+}
+
+int main(void) {
+
+    int user_option = 0;
     int choice = 1;
 
-    while (choice) {
+    print_menu();
 
-        std::cout << "Type in 1-6: ";
-        std::cin >> user_option;
-        std::cout << "Your choice: " << user_option << std::endl;
+    // looping the menu
+    while (choice != 0) {
 
-        switch (user_option) {
-            case 1:
-                std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers." << std::endl;
-                break;
-            case 2:
-                std::cout << "Market looks good." << std::endl;
-                break;
-            case 3:
-                std::cout << "Mark and offer - enter the amount." << std::endl;
-                break;
-            case 4:
-                std::cout << "Make a bid - enter the amount." << std::endl;
-                break;
-            case 5:
-                std::cout << "Your wallet is empty." << std::endl;
-                break;
-            case 6:
-                std::cout << "Going to next time frame." << std::endl;
-                break;
-            default:
-                std::cout << "Invalid choice. Choose from 1-6." << std::endl;
-                break;
-        }
+        // taking user input
+        user_option = get_user_option();
 
-        std::cout << "Enter 0 to exit or 1 to continue: ";
+        // processing the user input
+        process_user_option(user_option);
+
+        // prompting the user to check if they want to continue
+        std::cout << "Do you want to continue (1: yes; 0: no): ";
         std::cin >> choice;
-
-        if (choice == 0)
-            std::cout << "Exiting!!" << std::endl;
     }
 
     return 0;
