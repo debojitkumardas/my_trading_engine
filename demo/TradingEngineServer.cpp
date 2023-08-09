da44e5d8147a7e105dce786203acbdbfbd82539a
Debojit Kumar Das <debojit2997@gmail.com>
2023-08-09 21:05:44 +0530

    update

---
+++ demo/TradingEngineServer.cpp
@@ -0,0 +1,8 @@
+/*
+* TradingEngineServer namespace (need to create one)
+* Need to create a logger
+* Dependency injection??
+* Hosting??
+* appsettings.json
+* TradingEngineServer inherits from iTradingEnginerServer (privately)
+*/
---
+++ demo/iTradingEngineServer.cpp
@@ -0,0 +1,3 @@
+/*
+* Interface for our trading engine server
+*/
